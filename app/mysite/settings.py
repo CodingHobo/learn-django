ba64b5200f90fabba2b9eb546b7354d22bbcd65f
Crynowecer0 <crynowecer11@gmail.com>
2023-06-19 17:31:32 -0400

    progress on tutorial pt 2

--- app/mysite/settings.py
+++ app/mysite/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
+env = environ.Env()
+environ.Env.read_env() # read .env file
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,9 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "polls",
+        "USER": env('USER'),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
